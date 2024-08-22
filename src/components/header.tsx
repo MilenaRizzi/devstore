46bd210d48bcbddeb67c56c865287f96d3869259
Your Name <milenaclararizzi@gmail.com>
2024-08-22 14:38:49 -0300

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
-import { Search, ShoppingBag } from 'lucide-react'
 import Image from 'next/image'
+import CartWidget from './cart-widget'
+import { SearchForm } from './search-form'
 
 export default function Header() {
   return (
@@ -9,33 +10,22 @@ export default function Header() {
         <Link href="/" className="text-2xl font-extrabold text-white">
           devstore
         </Link>
-
-        <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-6 ring-zinc-700">
-          <Search className="w-5 h-5 text-zinc-500" />
-
-          <input
-            placeholder="Buscar produtos..."
-            type="text"
-            className="flex-1 bg-transparent text-sm outline-noe placeholder:text-zinc-500"
-          />
-        </form>
+        <SearchForm />
       </div>
       <div className="flex items-center gap-4">
-        <div className="flex items-center gap-2">
-          <ShoppingBag className="h-4 w-4" />
-          <span className="text-sm">Cart (0)</span>
+        <CartWidget />
+        <div className="w-px h-4 bg-zinc-700">
+          <Link href="/" className="flex items-center gap-2 hover:underline">
+            <span className="text-sm">Account</span>
+            <Image
+              src="https://github.com/MilenaRizzi.png"
+              className="h-6 w-6 rounded-full"
+              width={24}
+              height={24}
+              alt=""
+            />
+          </Link>
         </div>
-        <div className="w-px h-4 bg-zinc-700"></div>
-        <Link href="/" className="flex items-center gap-2 hover:underline">
-          <span className="text-sm">Account</span>
-          <Image
-            src="https://github.com/MilenaRizzi.png"
-            className="h-6 w-6 rounded-full"
-            width={24}
-            height={24}
-            alt=""
-          />
-        </Link>
       </div>
     </div>
   )

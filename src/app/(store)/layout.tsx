e559e6eb0442c08d3c3888846b129c2f397a09e6
Your Name <milenaclararizzi@gmail.com>
2024-08-24 18:05:54 -0300

    Update layout.tsx

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,15 +1,13 @@
 import Header from '@/components/header'
 import { CartProvider } from '@/contexts/cart-context'
-import { ReactNode, Suspense } from 'react'
+import { ReactNode } from 'react'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
     <CartProvider>
       <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-[min-content_max-content] gap-5 p-8">
-        <Suspense>
-          <Header />
-          {children}
-        </Suspense>
+        <Header />
+        {children}
       </div>
     </CartProvider>
   )

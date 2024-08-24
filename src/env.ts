42ff473267258db640a96a8c9ee9bd06281ae568
Your Name <milenaclararizzi@gmail.com>
2024-08-24 19:26:10 -0300

    add client/server env variables boundary

--- src/env.ts
+++ src/env.ts
@@ -1,23 +1,17 @@
-// arquivo para validar as variáveis ambiente. Valida que a aplicação está executando com as variáveis de preenchidas
-// instalamos o zod para validação
-
+// src/env.mjs
+import { createEnv } from '@t3-oss/env-nextjs'
 import { z } from 'zod'
 
-const envSchema = z.object({
-  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
-  APP_URL: z.string().url(),
+export const env = createEnv({
+  server: {
+    APP_URL: z.string().url(),
+  },
+  client: {
+    NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+  },
+
+  runtimeEnv: {
+    APP_URL: process.env.APP_URL,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
 })
-
-// pega o valor enviado como parâmetro (process.env) e vai fazer um parse, vai validar que o process.env está seguindo o formato do schema
-const parsedEnv = envSchema.safeParse(process.env)
-
-if (!parsedEnv.success) {
-  console.error(
-    'Invalid environment variables',
-    parsedEnv.error.flatten().fieldErrors,
-  )
-  // método flatten vai transformar os erros da validação em um formato mais legível
-  throw new Error('Invalid environment variables.')
-}
-
-export const env = parsedEnv.data
